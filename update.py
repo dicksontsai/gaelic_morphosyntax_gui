157ebef688aa6df57b536539af8c24b914cae528
dicksontsai <dickson.tsai@berkeley.edu>
2014-04-09 22:30:58 -0700

    Auto update script installed

--- update.py
+++ update.py
@@ -5,9 +5,15 @@ import urllib as url
 def update():
     response = tkMessageBox.askyesno("Confirmation for update", "Are you sure you want to update to the newest version?")
     if response:
-        internet_zipfile = url.urlopen("http://github.com/dicksontsai/gaelic_morphosyntax_gui/archive/master.zip")
-        zipfile_object = zipfile.ZipFile(internet_zipfile, 'r')
-        try:
-            zipfile_object.extractall(".")
-        except ZipFileException: 
-            tkMessageBox.showerror("Update Failed", "Sorry, the update failed")
+            url.urlretrieve("http://github.com/dicksontsai/gaelic_morphosyntax_gui/archive/master.zip", "morphosyntax_gui.zip")
+            print "Download of ZIP succeeded"
+            with open('morphosyntax_gui.zip', 'r') as gui_zip:
+                print "Opening"
+                zipfile_object = zipfile.ZipFile(gui_zip, 'r')
+                try:
+                    print "Extracting"
+                    zipfile_object.extractall("..")
+                    print "Update installed"
+                except Exception as e:
+                    print e 
+                    tkMessageBox.showerror("Update Failed", "Sorry, the update failed")
